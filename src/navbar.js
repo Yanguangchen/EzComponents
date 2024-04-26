2ce7c361ec05605a1111f756dcdbf5dcc769b021
Chen Yanguang <83961207+Yanguangchen@users.noreply.github.com>
2024-04-26 17:23:11 +0800

    commit navbar changes

--- src/navbar.js
+++ src/navbar.js
@@ -1,36 +1,45 @@
 import React, { useState } from "react";
+import companyLogo from "./Assets/webwizards.png";
 
 const topNavigation = {
-  backgroundColor: "#1D267D",
+  backgroundColor: "#00224D",
   overflow: "hidden",
   marginBottom: "2em",
 };
 
 const topNavigationA = {
-  float: "left",
-  display: "block",
+  display: "inline-block",
   color: "#f2f2f2",
   textAlign: "center",
-  padding: "14px 16px",
+  padding: "14px 40px",
   textDecoration: "none",
-  fontFamily: "roboto",
+  fontFamily: "League Spartan",
   ":hover": {
     backgroundColor: "#1D267D",
     color: "black",
   },
 };
 
+const navlinks = {
+  display: "flex",
+  justifyContent: "center",
+  borderLeft: "1px solid gray",
+  borderRoght: "1px solid gray",
+  fontSize: "1.5em",
+};
+
 const banner = {
-  backgroundColor: "#0C134F",
+  backgroundColor: "white",
   color: "white",
   margin: "0",
   padding: "0",
+  display: "flex",
+  justifyContent: "center",
+  borderTop: "1px solid #00224D",
 };
 
-const h1Div = {
-  margin: "0",
-  paddingBottom: "1em",
-  paddingTop: "3em",
+const logoStyle = {
+  width: "20%",
 };
 
 function NavbarComponent() {
@@ -39,22 +48,24 @@ function NavbarComponent() {
       <p id={"navibar"}></p>
 
       <div style={banner}>
-        <h1 style={h1Div}> COMPANY LOGO HERE</h1>
+        <img src={companyLogo} style={logoStyle} alt="Company Logo" />
       </div>
 
       <div style={topNavigation}>
-        <a style={topNavigationA} href="#home">
-          Home
-        </a>
-        <a style={topNavigationA} href="#news">
-          News
-        </a>
-        <a style={topNavigationA} href="#contact">
-          Contact
-        </a>
-        <a style={topNavigationA} href="#about">
-          About
-        </a>
+        <div style={navlinks}>
+          <a style={topNavigationA} href="#home">
+            Home
+          </a>
+          <a style={topNavigationA} href="#news">
+            News
+          </a>
+          <a style={topNavigationA} href="#contact">
+            Contact
+          </a>
+          <a style={topNavigationA} href="#about">
+            About
+          </a>
+        </div>
       </div>
     </div>
   );
